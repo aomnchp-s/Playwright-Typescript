5af29cb1b5fc14366573378b2ff1d0b8ccd0a7e3
aomnchp-s <natchaporn.aomnchp@gmail.com>
2024-03-24 23:23:17 +0700

    add test case, basic chain locator.

--- tests/demo-slector.spec.ts
+++ tests/demo-slector.spec.ts
@@ -12,3 +12,21 @@ test(
         console.log('Text: ' + userNameTxt);
     }
 );
+
+test(
+    'chain locator', async({page}) =>{
+        await page.goto('https://demoqa.com/login');
+        test.setTimeout(0);
+        //for enable debug mode.
+        await page.pause();
+    }
+);
+
+test(
+    'basic chain locator', async({page}) =>{
+        await page.goto('https://demoqa.com/login');
+        await page.fill('id=userName','aomnchp');
+        await page.fill('css=input[type="password"]','Aom@1234');
+        await page.click('id=userForm >> button >> nth=1');
+    }
+);
